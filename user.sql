f4c60dd6a63ca861e16bace30f92e22820cf0121
wlmwang <>
2022-11-24 00:01:43 +0800

    db sql

---
+++ user.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: test
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` bigint unsigned NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `extension` json DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1594601044528324609,'jack','{\"age\": 18, \"sex\": \"男\", \"addr\": \"上海浦东\", \"family\": [\"dad\", \"mum\", \"bro\"]}'),(1594601224120033282,'tom','{\"age\": 18, \"sex\": \"男\", \"addr\": \"上海浦东\", \"family\": [\"dad\", \"mum\", \"bro\"]}'),(1594601253257863170,'jerry','{\"age\": 18, \"sex\": \"男\", \"addr\": \"上海浦东\", \"family\": [\"dad\", \"mum\", \"bro\"]}'),(1594601416865079297,'张三','{\"age\": 20, \"sex\": \"男\", \"addr\": \"上海浦东\", \"family\": [\"dad\", \"mum\", \"bro\"]}'),(1594601457033928706,'李四','{\"age\": 30, \"sex\": \"男\", \"addr\": \"上海浦东\", \"family\": [\"dad\", \"mum\", \"bro\"]}'),(1594601540672544770,'王五','{\"age\": 30, \"sex\": \"男\", \"addr\": \"上海浦东新区\", \"family\": [\"dad\"]}'),(1594601676261810177,'小张','{\"age\": 50, \"sex\": \"男\", \"addr\": \"上海浦西\", \"family\": [\"dad\"]}'),(1594601729730797569,'小王','{\"age\": 25, \"sex\": \"男\", \"addr\": \"上海浦西\", \"family\": [\"dad\"]}'),(1594602039140409345,'小李','{\"age\": 28, \"sex\": \"男\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\"]}'),(1594606568514752513,'小红','{\"sex\": \"女\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1594930685881233410,'小红','{\"age\": 18, \"sex\": \"女\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1594931103705214978,'小红红','{\"age\": 18, \"sex\": \"女\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1594932637637685250,'小红红','{\"sex\": \"女\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1595075224763867137,'小桃','{\"sex\": \"女\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1595075224835170306,'小桃桃','{\"sex\": \"女\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1595076296244973570,'小桃','{\"sex\": \"女\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1595076296517603330,'小桃桃','{\"sex\": \"女\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1595080752575524865,'小红红','{\"sex\": \"女\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1595442207783014402,'小雪','{\"age\": 18, \"sex\": \"女\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1595444406281977858,'小熊1','{\"age\": 25, \"sex\": \"男\", \"addr\": \"上海\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}'),(1595444406286172161,'小熊2','{\"age\": 26, \"sex\": \"男\", \"addr\": \"北京\", \"phone\": \"10086\", \"family\": [\"dad\", \"mum\"]}');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-23 23:58:23
